447d1a0e5c0c1d3affc4f2a24253975916aba542
Bas Kamer <bas@bushbaby.nl>
2012-07-21 21:51:44 +0200

    CS Configuration to Config

--- Module.php
+++ Module.php
@@ -44,7 +44,7 @@ class Module
         );
     }
 
-    public function getServiceConfiguration()
+    public function getServiceConfig()
     {
         return array(
             'aliases' => array(
